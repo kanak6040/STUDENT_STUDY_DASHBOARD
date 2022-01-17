e10266b43f48a54f1c7f2f3fcd24c2e62b8bc0f6
Kanak Agrawal <akanak101@gmail.com>
2022-01-17 11:18:16 +0530

    completed Notes Tab

--- studentstudyportal/dashboard/models.py
+++ studentstudyportal/dashboard/models.py
@@ -5,3 +5,12 @@ from django.contrib.auth.models import User
 
 class Notes(models.Model):
     user= models.ForeignKey(User, on_delete=models.CASCADE)
+    title= models.CharField(max_length=200)
+    description= models.TextField()
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = "notes"
+        verbose_name_plural = "notes"
