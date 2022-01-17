5a6ea1088f5963b8cba359c92fee5908e737a191
Kanak Agrawal <akanak101@gmail.com>
2022-01-17 22:49:16 +0530

    working on dictionary and fixing bugs'

--- studentstudyportal/dashboard/urls.py
+++ studentstudyportal/dashboard/urls.py
@@ -3,15 +3,22 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+
     path('notes', views.notes, name="notes"),
     path('delete_notes/<int:pk>', views.delete_note, name="delete-note"),
     path('notes_detail/<int:pk>', views.NotesDetailView.as_view(), name="notes-detail"),
+
     path('homework', views.homework, name="homework"),
     path('update_homework/<int:pk>', views.update_homework, name="update-homework"),
     path('delete_homework/<int:pk>', views.delete_homework, name="delete-homework"),
+
     path('youtube', views.youtube, name="youtube"),
+
     path('todo', views.todo, name="todo"),
     path('update_todo/<int:pk>', views.update_todo, name="update-todo"),
     path('delete_todo/<int:pk>', views.delete_todo, name="delete-todo"),
-    path('book', views.books, name="books")
+
+    path('book', views.books, name="books"),
+
+    path('dictionary', views.dictionary, name="dictionary")
 ]
